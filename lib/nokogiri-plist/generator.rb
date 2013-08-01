5e7833f9f45f3ebecef69e760d528844170de2bc
fuuka <fuuka.t@gmail.com>
2013-08-01 16:07:52 +0900

    speed up generating plist.

--- lib/nokogiri-plist/generator.rb
+++ lib/nokogiri-plist/generator.rb
@@ -12,36 +12,40 @@ module NokogiriPList
       # Todo: make these procs and allow custom procs (for data for example)
       case value
       when Array
-          tag("array", nil, current_indent) do
-            value.inject("") do |result, item|
-              result + item.to_plist_xml_unchomped(current_indent + 1)
-            end
+        tag("array", nil, current_indent) do
+          result = ''
+          value.each do |item|
+            result << item.to_plist_xml_unchomped(current_indent + 1)
           end
-        when Hash
-          tag("dict", nil, current_indent) do
-            value.inject("") do |result, (dict_key, dict_value)|
-              result + tag("key", dict_key, current_indent + 1).chomp +
-              dict_value.to_plist_xml_unchomped
-            end
-          end
-        when TrueClass
-          tag("true", nil, current_indent)
-        when FalseClass
-          tag("false", nil, current_indent)
-        when Time
-          tag("date", value.utc.strftime('%Y-%m-%dT%H:%M:%SZ'), current_indent)
-        when Date # also catches DateTime
-          tag("date", value.strftime('%Y-%m-%dT%H:%M:%SZ'), current_indent)
-        when String, Symbol
-          tag("string", value, current_indent)
-        when Fixnum, Bignum, Integer
-          tag("integer", value, current_indent)
-        when Float
-          tag("real", value, current_indent)
-        when BigDecimal
-          tag("real", value.to_s('F'), current_indent)
+          result
         end
+      when Hash
+        tag("dict", nil, current_indent) do
+          result = ''
+          value.each do |dict_key, dict_value|
+            result << tag('key', dict_key, current_indent + 1).chomp
+            result << dict_value.to_plist_xml_unchomped
+          end
+          result
+        end
+      when TrueClass
+        tag("true", nil, current_indent)
+      when FalseClass
+        tag("false", nil, current_indent)
+      when Time
+        tag("date", value.utc.strftime('%Y-%m-%dT%H:%M:%SZ'), current_indent)
+      when Date # also catches DateTime
+        tag("date", value.strftime('%Y-%m-%dT%H:%M:%SZ'), current_indent)
+      when String, Symbol
+        tag("string", value, current_indent)
+      when Fixnum, Bignum, Integer
+        tag("integer", value, current_indent)
+      when Float
+        tag("real", value, current_indent)
+      when BigDecimal
+        tag("real", value.to_s('F'), current_indent)
       end
+    end
 
     def self.indent(number)
       " " * (number * indent_size)
